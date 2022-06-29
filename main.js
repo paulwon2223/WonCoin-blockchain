4dbdeee2e29fdbbed2f59f9978ab8bc3cbdf120e
Paul Won <wonpaul2223@gmail.com>
2022-06-29 15:53:08 -0500

    implemented proof of work

--- main.js
+++ main.js
@@ -13,11 +13,25 @@ class Block {
         this.data = data;
         this.previousHash = previousHash;
         this.hash = this.getHash();
+        // nonce value is a random number that does not effect the chain if changed
+        this.nonce = 0;
     }
 
     // will obtain the hash of the block
     getHash() {
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+    }
+
+    // implementing Proof of Work, puts limit to how many blocks can be created, 
+        // requires hash of the block to start with certain amount of zeros
+    mineBlock(difficulty) {
+        //  will set the first set of numbers (amount depending on the difficulty set) and will produce zeros infront
+        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+            this.nonce++;
+            this.hash = this.getHash();
+        }
+
+        console.log(`Block Mined: ${this.hash}`);
     }
 };
 
@@ -26,6 +40,8 @@ class BlockChain{
     constructor() {
         // initializes the blockchain
         this.chain = [this.getGenesisBlock()];
+        // raising the difficulty will take more time for blocks to be created
+        this.difficulty = 4;
     }
 
     // create the first block (aka genesis block) of the blockchain, must be manually created 
@@ -45,7 +61,7 @@ class BlockChain{
         newBlock.previousHash = this.getRecentBlock().hash;
 
         // updates the hash anytime the properites of the block is changed
-        newBlock.hash = newBlock.getHash();
+        newBlock.mineBlock(this.difficulty)
 
         // pushes the new block to the chain
         this.chain.push(newBlock);
@@ -75,9 +91,13 @@ class BlockChain{
 
 // creates a coin called wonCoin, and adds multiple block to its chain 
 let wonCoin = new BlockChain();
+
+console.log('Mining Block 1...');
 wonCoin.addBlock(new Block(1, "06/08/2022", { amount: 4}));
+
+console.log('Mining Block 2...');
 wonCoin.addBlock(new Block(1, "06/11/2022", { amount: 10}));
 
-console.log(`Is blockchain of wonCoin valid? ${wonCoin.validateChain()}`);
+// console.log(`Is blockchain of wonCoin valid? ${wonCoin.validateChain()}`);
 
-console.log(JSON.stringify(wonCoin, null, 4));
+// console.log(JSON.stringify(wonCoin, null, 4));
